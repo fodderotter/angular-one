5e4d6e970d9b71c1e495d68481c7dd01cf058685
Ryan Walsh <ryan@thehatrack.net>
2016-02-22 09:29:01 -0600

    initial commit

---
+++ inventoryCtrl.js
@@ -0,0 +1,14 @@
+var items = [
+	  {
+		  name: 'rotten log'
+		, price: 12.99
+	  }
+	, {
+		  name: 'single chipped brick'
+		, price: 45
+	  }
+	, {
+		  name: 'left shoe, size 15'
+		, price: 25
+	  }
+];
